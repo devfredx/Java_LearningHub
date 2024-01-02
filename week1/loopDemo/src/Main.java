b0964a09a5b4db30b2edd4ea762e5fa85c516b7f
ferhatsusam <dev.fredx@gmail.com>
2024-01-02 22:01:46 +0300

    demo for while, for, do-while loops

---
+++ week1/loopDemo/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i < 10; i += 2)
+            System.out.println(i);
+
+        System.out.println(" - for loop completed\n\n");
+
+
+        int i = 1;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println(" - while loop completed \n\n");
+
+
+        int j = 100;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j < 10);
+        System.out.println(" - do-while loop completed");
+
+
+    }
+}
