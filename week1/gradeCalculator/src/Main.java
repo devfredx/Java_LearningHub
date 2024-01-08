0eddbdaba75e50141f2a9331ae28759747251cdf
ferhatsusam <dev.fredx@gmail.com>
2024-01-08 21:07:30 +0300

    feat: add the code to calculate the average grade per user lesson number

--- week1/gradeCalculator/src/Main.java
+++ week1/gradeCalculator/src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int[] grades = new int[numSubjects];
+        int sum = 0;
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter the grade for Subject " + (i + 1) + ": ");
+            grades[i] = scanner.nextInt();
+            sum += grades[i];
+        }
+
+        double average = (double) sum / numSubjects;
+
+        char letterGrade;
+        if (average >= 90) {
+            letterGrade = 'A';
+        } else if (average >= 80) {
+            letterGrade = 'B';
+        } else if (average >= 70) {
+            letterGrade = 'C';
+        } else if (average >= 60) {
+            letterGrade = 'D';
+        } else {
+            letterGrade = 'F';
+        }
+
+        // Display results
+        System.out.println("\nAverage Grade: " + average);
+        System.out.println("Letter Grade: " + letterGrade);
+        scanner.close();
     }
 }
