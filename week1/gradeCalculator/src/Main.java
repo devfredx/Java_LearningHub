f03f2ffce2c15068451d7b142398374423bc3451
ferhatsusam <dev.fredx@gmail.com>
2024-01-08 21:36:18 +0300

    feat: update the code and interface to prevent unwanted user input

--- week1/gradeCalculator/src/Main.java
+++ week1/gradeCalculator/src/Main.java
@@ -1,18 +1,19 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the number of subjects: ");
-        int numSubjects = scanner.nextInt();
 
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = getInputInt(scanner);
         int[] grades = new int[numSubjects];
         int sum = 0;
 
         for (int i = 0; i < numSubjects; i++) {
             System.out.print("Enter the grade for Subject " + (i + 1) + ": ");
-            grades[i] = scanner.nextInt();
+            grades[i] = getInputGrade(scanner);
             sum += grades[i];
         }
 
@@ -30,10 +31,42 @@ public class Main {
         } else {
             letterGrade = 'F';
         }
-
-        // Display results
         System.out.println("\nAverage Grade: " + average);
         System.out.println("Letter Grade: " + letterGrade);
         scanner.close();
+    }
+
+    private static int getInputInt(Scanner scanner) {
+        int input;
+        while (true) {
+            try {
+                input = scanner.nextInt();
+                if (input > 0) {
+                    return input;
+                } else {
+                    System.out.print("Please enter a positive integer. Try again.\n: ");
+                }
+            } catch (InputMismatchException e) {
+                System.out.print("Please enter a valid integer. Try again.\n: ");
+                scanner.next();
+            }
+        }
+    }
+
+    private static int getInputGrade(Scanner scanner) {
+        int input;
+        while (true) {
+            try {
+                input = scanner.nextInt();
+                if (input >= 0 && input <= 100) {
+                    return input;
+                } else {
+                    System.out.print("Please enter a grade between 0 and 100. Try again.\n: ");
+                }
+            } catch (InputMismatchException e) {
+                System.out.print("Please enter a valid integer. Try again.\n: ");
+                scanner.next();
+            }
+        }
     }
 }
