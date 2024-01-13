52a739928dc105d1f3a23f7718cc3ce87af248cd
ferhatsusam <dev.fredx@gmail.com>
2024-01-13 19:55:21 +0300

    explore getter and setter constructs

--- week2/classesWithAttributes/src/Main.java
+++ week2/classesWithAttributes/src/Main.java
@@ -5,22 +5,16 @@ public class Main {
         Product product = new Product();
 
         product.name = "Laptop";
-        product.id = 1;
-        product.description = "Asus Laptop";
-        product.price = 5000;
-        product.stockAmount = 3;
+        product.setId(1);
+        product.setDescription("Asus Laptop");
+        product.setPrice(5000);
+        product.setStockAmount(3);
+
 
         ProductManager productManager = new ProductManager();
         productManager.Add(product);
 
-        /*
-        productManager.Add2(1, "", "", 2, 200);
-        productManager.Add3(1, "", "", 2, 200);
-        productManager.Add4(1, "", "", 2, 200);
-        productManager.Add5(1, "", "", 2, 200);
-        productManager.Add6(1, "", "", 2, 200);
-        productManager.Add7(1, "", "", 2, 200);
-        */
+        System.out.println(product.getCode());
 
 
     }
