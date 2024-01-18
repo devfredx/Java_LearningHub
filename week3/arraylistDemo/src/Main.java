2e9ec16ccdcc312f8568d47a6a814cc0d1db2b8f
ferhatsusam <dev.fredx@gmail.com>
2024-01-18 22:08:07 +0300

    getting started with arraylist

---
+++ week3/arraylistDemo/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList numbers new ArrayList ();
+        numbers.add (1);
+        numbers.add(10);
+        numbers.add("Ankara");
+
+        // System.out.println (numbers.size());
+
+        // numbers.set (2,100);
+
+        // numbers.remove(index: 0);
+        // numbers.clear();
+        // System.out.println (numbers.get(0));
+
+        for (Object i :numbers){
+            System.out.println(i);
+        }
+
+    }
+}
