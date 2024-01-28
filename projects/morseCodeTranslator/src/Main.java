a0326fcaa17f0ae543e38d1575edbf0662425e75
ferhatsusam <dev.fredx@gmail.com>
2024-01-28 13:19:40 +0300

    feat: add morse codes for all letters

--- projects/morseCodeTranslator/src/Main.java
+++ projects/morseCodeTranslator/src/Main.java
@@ -68,7 +68,30 @@ public class Main {
         morseCodeMap.put('A', ".-");
         morseCodeMap.put('B', "-...");
         morseCodeMap.put('C', "-.-.");
-        
+        morseCodeMap.put('D', "-..");
+        morseCodeMap.put('E', ".");
+        morseCodeMap.put('F', "..-.");
+        morseCodeMap.put('G', "--.");
+        morseCodeMap.put('H', "....");
+        morseCodeMap.put('I', "..");
+        morseCodeMap.put('J', ".---");
+        morseCodeMap.put('K', "-.-");
+        morseCodeMap.put('L', ".-..");
+        morseCodeMap.put('M', "--");
+        morseCodeMap.put('N', "-.");
+        morseCodeMap.put('O', "---");
+        morseCodeMap.put('P', ".--.");
+        morseCodeMap.put('Q', "--.-");
+        morseCodeMap.put('R', ".-.");
+        morseCodeMap.put('S', "...");
+        morseCodeMap.put('T', "-");
+        morseCodeMap.put('U', "..-");
+        morseCodeMap.put('V', "...-");
+        morseCodeMap.put('W', ".--");
+        morseCodeMap.put('X', "-..-");
+        morseCodeMap.put('Y', "-.--");
+        morseCodeMap.put('Z', "--..");
+
 
         for (char key : morseCodeMap.keySet()) {
             charMap.put(morseCodeMap.get(key), key);
