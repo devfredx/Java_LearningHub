1c8579851e3aacfff920b64e9580c28080d056d8
ferhatsusam <dev.fredx@gmail.com>
2024-01-29 18:51:22 +0300

    feat: create QuestionBank.java file, add java quiz questions and answers

---
+++ projects/javaQuiz/src/QuestionBank.java
@@ -0,0 +1,11 @@
+public class QuestionBank {
+    public static String[] questions = {
+            "1. What is the correct way to declare a variable in Java?\n\tA. int myVariable;\n\tB. variable myVariable;\n\tC. myVariable = int;\n\tD. declare myVariable as int;\n\tE. None of the above.\n",
+            "2. What is the result of 3 + 5 * 2?\n\tA. 16\n\tB. 13\n\tC. 11\n\tD. 10\n\tE. None of the above.\n",
+            "3. Which of the following is NOT a primitive data type in Java?\n\tA. int\n\tB. double\n\tC. string\n\tD. char\n\tE. boolean\n",
+            "4. What keyword is used to define a class in Java?\n\tA. class\n\tB. interface\n\tC. struct\n\tD. object\n\tE. None of the above.\n",
+            "5. What does the 'static' keyword mean in Java?\n\tA. It makes a variable constant.\n\tB. It allows a method to access instance variables.\n\tC. It indicates that a method or variable belongs to the class itself, not instances of the class.\n\tD. It prevents inheritance.\n\tE. None of the above.\n"
+    };
+
+    public static String[] answers = {"A", "B", "C", "A", "C"};
+}
