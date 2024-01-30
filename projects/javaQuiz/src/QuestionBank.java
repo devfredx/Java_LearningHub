3fb8d467e3efed38f425bad2d5e3c84c6c5879c5
ferhatsusam <dev.fredx@gmail.com>
2024-01-30 20:30:54 +0300

    refactor: reorganize QuestionBank to use the Question class - modify QuestionBank to use the Question class to store questions, options and correct answers.

--- projects/javaQuiz/src/QuestionBank.java
+++ projects/javaQuiz/src/QuestionBank.java
@@ -1,11 +1,22 @@
 public class QuestionBank {
-    public static String[] questions = {
-            "1. What is the correct way to declare a variable in Java?\n\tA. int myVariable;\n\tB. variable myVariable;\n\tC. myVariable = int;\n\tD. declare myVariable as int;\n\tE. None of the above.\n",
-            "2. What is the result of 3 + 5 * 2?\n\tA. 16\n\tB. 13\n\tC. 11\n\tD. 10\n\tE. None of the above.\n",
-            "3. Which of the following is NOT a primitive data type in Java?\n\tA. int\n\tB. double\n\tC. string\n\tD. char\n\tE. boolean\n",
-            "4. What keyword is used to define a class in Java?\n\tA. class\n\tB. interface\n\tC. struct\n\tD. object\n\tE. None of the above.\n",
-            "5. What does the 'static' keyword mean in Java?\n\tA. It makes a variable constant.\n\tB. It allows a method to access instance variables.\n\tC. It indicates that a method or variable belongs to the class itself, not instances of the class.\n\tD. It prevents inheritance.\n\tE. None of the above.\n"
+    public static Question[] questions = {
+            new Question("1. What is the correct way to declare a variable in Java?",
+                    new String[]{"A. int myVariable;", "B. variable myVariable;", "C. myVariable = int;",
+                            "D. declare myVariable as int;", "E. None of the above."},
+                    "A"),
+            new Question("2. What is the result of 3 + 5 * 2?",
+                    new String[]{"A. 16", "B. 13", "C. 11", "D. 10", "E. None of the above."},
+                    "B"),
+            new Question("3. Which of the following is NOT a primitive data type in Java?",
+                    new String[]{"A. int", "B. double", "C. string", "D. char", "E. boolean"},
+                    "C"),
+            new Question("4. What keyword is used to define a class in Java?",
+                    new String[]{"A. class", "B. interface", "C. struct", "D. object", "E. None of the above."},
+                    "A"),
+            new Question("5. What does the 'static' keyword mean in Java?",
+                    new String[]{"A. It makes a variable constant.", "B. It allows a method to access instance variables.",
+                            "C. It indicates that a method or variable belongs to the class itself, not instances of the class.",
+                            "D. It prevents inheritance.", "E. None of the above."},
+                    "C")
     };
-
-    public static String[] answers = {"A", "B", "C", "A", "C"};
 }
