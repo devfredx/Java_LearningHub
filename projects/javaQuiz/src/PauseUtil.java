b5fc81cb08fb890d31597f1e742e2f9c5216df51
ferhatsusam <dev.fredx@gmail.com>
2024-01-31 19:06:07 +0300

    feat: add a PauseUtil class with a pause method to add delays to the program

---
+++ projects/javaQuiz/src/PauseUtil.java
@@ -0,0 +1,9 @@
+public class PauseUtil {
+    public static void pause(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
