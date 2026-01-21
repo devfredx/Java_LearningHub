07e03758cc819ec6233f3eebde36767b0bff611b
devfredx <dev.fredx@gmail.com>
2026-01-21 19:57:18 +0300

    feat: add Java scope examples for class method and block levels

---
+++ 02_Methods_and_Logic/javaScope/src/Main.java
@@ -0,0 +1,27 @@
+public class JavaScope {
+
+    static int classLevelVariable = 100;
+
+    public static void main(String[] args) {
+
+        int methodLevelVariable = 50;
+
+        System.out.println("Class variable in main: " + classLevelVariable);
+        System.out.println("Method variable in main: " + methodLevelVariable);
+
+        if (methodLevelVariable > 10) {
+            int blockLevelVariable = 25;
+            System.out.println("Accessing method variable in block: " + methodLevelVariable);
+            System.out.println("Block variable: " + blockLevelVariable);
+        }
+
+        displayOtherScope();
+    }
+
+    public static void displayOtherScope() {
+        int anotherMethodVariable = 10;
+
+        System.out.println("Class variable in another method: " + classLevelVariable);
+        System.out.println("Variable local to this method: " + anotherMethodVariable);
+    }
+}
