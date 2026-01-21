2b85c31e35d4e091be044c2b2aaa481c904b0171
devfredx <dev.fredx@gmail.com>
2026-01-21 20:06:01 +0300

    feat: add examples of public, private, protected, and default access modifiers

---
+++ 03_Object_Oriented_Programming/accessModifiers/src/Main.java
@@ -0,0 +1,24 @@
+class DataVault {
+    public String publicData = "Public: Accessible everywhere";
+    protected String protectedData = "Protected: Same package and subclasses";
+    String defaultData = "Default: Same package only";
+    private String privateData = "Private: This class only";
+
+    public void displayPrivateData() {
+        System.out.println("Accessing from within the class: " + privateData);
+    }
+}
+
+public class AccessModifiers {
+    public static void main(String[] args) {
+        DataVault vault = new DataVault();
+
+        System.out.println(vault.publicData);
+        System.out.println(vault.protectedData);
+        System.out.println(vault.defaultData);
+
+        vault.displayPrivateData();
+
+        System.out.println("Private data cannot be accessed directly: vault.privateData (Error)");
+    }
+}
